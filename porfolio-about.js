8a01c648d1ed267f004a31ddc63f783492a62988
songjoonhyeok <dnvkds@naver.com>
2017-06-20 16:32:50 +0900

    서브페이지1완

---
+++ porfolio-about.js
@@ -0,0 +1,53 @@
+$(function(){
+
+  var events = function(){
+
+      var ww = $(this).width();
+      console.log(ww)
+
+      if(ww > 1030 ){
+          //pc
+          // 창가로  크기가 1030 미만 일경우
+          $('.nav>ul>li').mouseenter(function(){
+              $('.nav>ul>li').children('div').stop().slideDown()
+
+          }).mouseleave(function(){
+            $('.nav>ul>li').children('div').stop().slideUp()
+
+          })
+      } else if(ww <= 1030 ){
+        //mobile
+        // 창가로  크기가 1030 클 경우
+        $('.nav>ul>li').mouseenter(function(){
+            $(this).children('div').stop().slideDown()
+
+        }).mouseleave(function(){
+            $(this).children('div').stop().slideUp()
+
+        })
+
+      }
+
+  }
+  $(window).resize(events)
+   $(window).trigger('resize')
+
+
+        $(window).scroll(function(){
+           var src = $(this).scrollTop();
+
+           if (src >= 70){
+
+             $('.nav').css({
+               'marginTop':0
+             })
+           }else if (src <= 70){
+             $('.nav').css({
+               'marginTop':'70px'
+             })
+           }
+        })
+
+
+
+})
