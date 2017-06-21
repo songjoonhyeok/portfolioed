413f60bdc6a3b59f9decbfa7d319f4b3b37a0860
songjoonhyeok <dnvkds@naver.com>
2017-06-21 18:23:22 +0900

    오전,오후 푸쉬

--- porfolio-product.js
+++ porfolio-product.js
@@ -50,4 +50,146 @@ $(function(){
 
 
 
+
+
+
+
+        $('.product_menu li a').click(function(e){
+            e.preventDefault();
+
+
+
+            var item =  $(this).attr('id');
+            console.log(item);
+
+            var idx= $(this).pa
+
+            $('.product_photo li').each(function(i,e){
+
+
+                if(item == $(e).find('img').attr('alt')){
+                  $(e).show()
+                } else{
+                  $(e).hide()
+                }
+
+
+            })
+
+        })
+
+
+
+          // var menu = {
+          //     "food":
+          //         [{
+          //             "url":"../스크롤&스크린/images/1.jpg",
+          //             "menuName":"스테이크",
+          //             "price":"12,000",
+          //             "time":"dinner"
+          //         },{
+          //             "url":"../스크롤&스크린/images/2.jpg",
+          //             "menuName":"스테이크",
+          //             "price":"13,000",
+          //             "time":"dinner"
+          //         },{
+          //             "url":"../스크롤&스크린/images/3.jpg",
+          //             "menuName":"스테이크",
+          //             "price":"14,000",
+          //             "time":"dinner"
+          //         },{
+          //             "url":"../스크롤&스크린/images/4.jpg",
+          //             "menuName":"돼지갈비정식",
+          //             "price":"10,000",
+          //             "time":"dinner"
+          //         },{
+          //             "url":"../스크롤&스크린/images/5.jpg",
+          //             "menuName":"스파게티",
+          //             "price":"9,000",
+          //             "time":"lunch"
+          //         },{
+          //             "url":"../스크롤&스크린/images/6.jpg",
+          //             "menuName":"스파게티",
+          //             "price":"9,900",
+          //             "time":"lunch"
+          //         },{
+          //             "url":"../스크롤&스크린/images/7.jpg",
+          //             "menuName":"스파게티",
+          //             "price":"11,000",
+          //             "time":"lunchr"
+          //         },{
+          //             "url":"../스크롤&스크린/images/8.jpg",
+          //             "menuName":"오리스테이크",
+          //             "price":"12,000",
+          //             "time":"lunch"
+          //         },{
+          //             "url":"../스크롤&스크린/images/9.jpg",
+          //             "menuName":"피자",
+          //             "price":"10,000",
+          //             "time":"dinner"
+          //         },{
+          //             "url":"../스크롤&스크린/images/10.jpg",
+          //             "menuName":"빵",
+          //             "price":"1,500",
+          //             "time":"brunch"
+          //         },{
+          //             "url":"../스크롤&스크린/images/11.jpg",
+          //             "menuName":"아이스커피",
+          //             "price":"2,000",
+          //             "time":"drink"
+          //         },{
+          //             "url":"../스크롤&스크린/images/d1.jpg",
+          //             "menuName":"칵테일",
+          //             "price":"5,000",
+          //             "time":"drink"
+          //         },{
+          //             "url":"../스크롤&스크린/images/d2.jpg",
+          //             "menuName":"칵테일",
+          //             "price":"5,000",
+          //             "time":"drink"
+          //         },{
+          //             "url":"../스크롤&스크린/images/d3.jpg",
+          //             "menuName":"칵테일",
+          //             "price":"5,000",
+          //             "time":"drink"
+          //         },{
+          //             "url":"../스크롤&스크린/images/d4.jpg",
+          //             "menuName":"칵테일",
+          //             "price":"5,000",
+          //             "time":"drink"
+          //         },{
+          //             "url":"../스크롤&스크린/images/d5.jpg",
+          //             "menuName":"칵테일",
+          //             "price":"5,000",
+          //             "time":"drink"
+          //         }]
+          // }
+          //
+          // $.each(menu.food,function(i,e){
+          //
+          //   var a = "";
+          //
+          //   a += "<li class='"+e.time+" all'>";
+          //   a += "<img src='"+e.url+"'/>"
+          //   a += "<span>"+e.menuName+"<span>";
+          //   a += "<span>"+e.price+"<span>";
+          //   a += "</li>";
+          //
+          //   $('.list').append(a)
+          //
+        // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
