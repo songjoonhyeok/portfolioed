91c2d1a3e3289593d41ba14ebc0575d31e6c6987
songjoonhyeok <dnvkds@naver.com>
2017-06-27 18:20:14 +0900

    화요일 작업분량

---
+++ porfolio-js/commtnity-sub1.js
@@ -0,0 +1,65 @@
+$(function(){
+
+  $('.nav>ul>li').hover(function(){
+    $('.nav>ul>li').children('div').stop().slideDown()
+  },function(){
+    $('.nav>ul>li').children('div').stop().slideUp()
+  })
+
+
+          $(window).scroll(function(){
+             var src = $(this).scrollTop();
+
+             if (src >= 70){
+
+               $('.nav').css({
+                 'marginTop':0
+               })
+             }else if (src <= 70){
+               $('.nav').css({
+                 'marginTop':'70px'
+               })
+             }
+          })
+
+        //sub 서브 카테고리란
+          $('.sub-nav .sub1').hover(function(){
+             $('.sub-nav .sub1').css({
+                'background-color':'#ecefce',
+                'border':'3px #a8e5cc solid'
+             }).find('a').css("color",'#131313')
+          },function(){
+            $('.sub-nav .sub1').css({
+              'background-color':'#ffd6d6',
+              'border':'3px #ffa7a7 solid'
+            }).find('a').css("color",'')
+          })
+          $('.sub-nav .sub2').hover(function(){
+             $('.sub-nav .sub2').css({
+                'background-color':'#ecefce',
+                'border':'3px #a8e5cc solid'
+             }).find('a').css("color",'#131313')
+          },function(){
+            $('.sub-nav .sub2').css({
+              'background-color':'#ffd6d6',
+              'border':'3px #ffa7a7 solid'
+            }).find('a').css("color",'')
+          })
+          $('.sub-nav .sub3').hover(function(){
+             $('.sub-nav .sub3').css({
+                'background-color':'#ecefce',
+                'border':'3px #a8e5cc solid'
+             }).find('a').css("color",'#131313')
+          },function(){
+            $('.sub-nav .sub3').css({
+              'background-color':'#ffd6d6',
+              'border':'3px #ffa7a7 solid'
+            }).find('a').css("color",'')
+          })
+
+
+
+
+
+
+})
--- porfolio-js/shopping-bag.js
+++ porfolio-js/shopping-bag.js
@@ -33,5 +33,25 @@ $(function(){
     })
   })
 
+          $(window).scroll(function(){
+             var src = $(this).scrollTop();
+
+             if (src >= 70){
+
+               $('.nav').css({
+                 'marginTop':0
+               })
+             }else if (src <= 70){
+               $('.nav').css({
+                 'marginTop':'70px'
+               })
+             }
+          })
+
+
+
+
+
+
 
 })
