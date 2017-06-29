06741174248af377ff2642d94157a2723eb6c3bf
songjoonhyeok <dnvkds@naver.com>
2017-06-29 16:51:30 +0900

    포폴작업 이벤트 페이지만 남았음

--- porfolio-js/portfolio.js
+++ porfolio-js/portfolio.js
@@ -4,7 +4,7 @@ $(function(){
    },function(){
      $('.nav>ul>li').children('div').stop().slideUp()
    })
-     
+
 
   //------------------------------------윈도우 리사이징
   // var events = function(){
@@ -116,7 +116,7 @@ $(function(){
   $('.slidebanner li').eq(idx).addClass('on').siblings().removeClass('on')
 
          }
-      // console.log(idx);
+      console.log(idx);
     })
     $('.slide li').mouseenter(function(){
                clearInterval(tt);
